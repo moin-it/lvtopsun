012eb9562765da92454f14f4ec5ab1efc5e22150
Moin Siddique <mgl.moin@gmail.com>
2026-06-30 14:37:24 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_NAME = 'pwa-cache-v2';
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(['./']); }));
   self.skipWaiting();
