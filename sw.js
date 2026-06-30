cb02337bd8b5e337c469a4154da679215a4ac0a5
Moin Siddique <mgl.moin@gmail.com>
2026-06-30 14:51:18 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-const CACHE_NAME = 'pwa-cache-v2';
+const CACHE_NAME = 'pwa-cache-v3';
+const PRECACHE_URLS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
 self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(['./']); }));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(c) {
+      return Promise.all(
+        PRECACHE_URLS.map(function(url) {
+          return c.add(url).catch(function(err) {
+            console.warn('Precache failed for', url, err);
+          });
+        })
+      );
+    })
+  );
   self.skipWaiting();
 });
+
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(names) {
@@ -11,6 +24,7 @@ self.addEventListener('activate', function(e) {
   );
   self.clients.claim();
 });
+
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(cached) {
